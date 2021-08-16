aae28d8355fedf574568083a41ab90814fc87825
ericbsantana <bitencourteric@hotmail.com>
2021-08-16 11:54:14 -0300

    add toggle cart

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import Main from "./components/Main";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [cart, setCart] = useState("below");
+
+  const toggleCart = () => {
+    if (cart === "below") {
+      setCart("above");
+    } else {
+      setCart("below");
+    }
+  };
   return (
-    <div className="flex justify-center items-center h-screen w-screen bg-gray-200 font-poppins">
-      <Main />
+    <div className="flex flex-col justify-center items-center bg-gray-200 min-h-screen font-poppins">
+      <button
+        className="bg-blue-500 text-white shadow-md px-5 py-2 m-5 rounded-md font-bold hover:bg-blue-600 active:bg-blue-700"
+        onClick={toggleCart}
+      >
+        Change between carts
+      </button>
+      <Main cart={cart} />
     </div>
   );
-}
+};
 
 export default App;
