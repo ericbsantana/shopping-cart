3d49d0b47c80661bb7483377e9dd0cecc7ad0534
ericbsantana <bitencourteric@hotmail.com>
2021-08-16 13:54:35 -0300

    rmv proxy

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const above = require("./data/acima10.json");
 const below = require("./data/abaixo10.json");
-
 const express = require("express");
 const router = express.Router();
 const app = express();
 const port = process.env.PORT || 3001;
 const cors = require("cors");
+const path = require("path");
+
 router.get("/above", (req, res) => {
   res.send(JSON.stringify(above));
 });
@@ -16,6 +17,14 @@ router.get("/below", (req, res) => {
 
 app.use(cors());
 app.use(router);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Listening at ${port}`);
