08ff52aafbcd0059928164252e30d64b1bc541bc
ericbsantana <bitencourteric@hotmail.com>
2021-08-16 11:54:43 -0300

    add icon, loading and error states

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,15 +1,19 @@
 const CartItem = (props) => {
   return (
     <li className="flex items-center p-5 w-full space-x-3">
-      <span className="w-32 border border-gray-400">
-        <img src={props.img} alt={props.name} className="object-cover" />
+      <span className="w-1/3  ">
+        <img
+          src={props.img}
+          alt={props.name}
+          className="w-24 h-24 object-center object-cover border border-gray-400"
+        />
       </span>
-      <span>
+      <span className="w-full">
         <h1 className="w-full font-bold uppercase text-md">{props.name}</h1>
-        <h1 className="w-full text-gray-500 text-sm">
-          R$ {props.originalPrice}
+        <h1 className="w-full text-gray-400 text-xs line-through">
+          {props.originalPrice}
         </h1>
-        <h1 className="w-full">R$ {props.price}</h1>
+        <h1 className="w-full">{props.price}</h1>
       </span>
     </li>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,40 +1,72 @@
 import CartItem from "./CartItem";
-import axios from "axios";
 import { useState, useEffect } from "react";
+
+import axios from "axios";
+import currency from "currency.js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleNotch, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const Main = (props) => {
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState([]);
-  const [error, setError] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         setLoading(true);
-        const response = await axios.get("http://localhost:3001/below");
+        const response = await axios.get(`http://localhost:3001/${props.cart}`);
+
         setData(response.data);
         setLoading(false);
       } catch (e) {
         setLoading(false);
         setError(true);
-        console.log(e);
       }
     };
     fetchData();
-  }, []);
+  }, [props.cart]);
 
   const calculateTotal = () => {
     let total = 0;
-    data.items.map((item) => (total += item.sellingPrice / 100));
-    return total;
+    data.items.map((item) => (total += item.sellingPrice));
+    return BRL(total);
   };
 
+  const BRL = (value) =>
+    currency(value / 100, {
+      symbol: "R$",
+      precision: 2,
+      decimal: ",",
+      separator: ".",
+    });
+
   return (
-    <div className="lg:w-1/4 shadow-xl rounded-lg bg-white ">
+    <div className="lg:w-1/3 shadow-xl rounded-lg bg-white mb-10 ">
       <h1 className="lg:w-full py-2 text-lg text-center border-b font-bold">
         Meu carrinho
       </h1>
       <div className="lg:w-full">
+        {loading && (
+          <div className="flex flex-col w-full justify-center content-center items-center space-y-3 my-5">
+            <FontAwesomeIcon
+              icon={faCircleNotch}
+              className="animate-spin"
+              size={"2x"}
+            />
+            <span className="text-2xl">Loading...</span>
+          </div>
+        )}
+        {error && (
+          <div className="flex flex-col w-full justify-center content-center items-center space-y-3 my-5">
+            <FontAwesomeIcon
+              icon={faTimes}
+              className="animate-spin"
+              size={"2x"}
+            />
+            <span className="text-2xl">Loading...</span>
+          </div>
+        )}
         <ul className="border-b padding-5 border-gray-200">
           {!loading &&
             data.items.map((item) => (
@@ -42,22 +74,30 @@ const Main = (props) => {
                 key={item.uniqueId}
                 img={item.imageUrl}
                 name={item.name}
-                originalPrice={item.price / 100}
-                price={item.sellingPrice / 100}
-                total={item.total}
+                originalPrice={BRL(item.price).format()}
+                price={BRL(item.sellingPrice).format()}
               />
             ))}
         </ul>
         {!loading && (
           <div className="flex justify-between font-bold m-5">
             <span>Total:</span>
-            <span>{calculateTotal()}</span>
+            <span>{calculateTotal().format()}</span>
           </div>
         )}
+        {!loading && calculateTotal().dollars() > 10 && (
+          <div className="flex flex-col justify-center content-center items-center">
+            <span className="w-3/4 rounded-full bg-green-200 text-green-800 py-2 px-3 m-5 text-center">
+              Parabéns, sua compra tem frete grátis!
+            </span>
+          </div>
+        )}
+        <div className="w-full border-b border-gray-200"></div>
         <div className="flex justify-center">
           <button
             type="button"
-            className="bg-blue-500 text-white shadow-md px-5 py-2 m-5 w-full rounded-md"
+            className="w-full bg-blue-500 text-white shadow-md px-5 py-2 m-5 rounded-md font-bold hover:bg-blue-600 active:bg-blue-700"
+            disabled={loading}
           >
             Finalizar compra
           </button>
